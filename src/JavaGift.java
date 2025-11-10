16161919b106242b08ac308d38be57d29187a161
gui <gui151208@gmail.com>
2025-11-10 20:11:09 -0300

    mooc

---
+++ src/JavaGift.java
@@ -0,0 +1,32 @@
+public class JavaGift {
+    static void main() {
+        Gift book = new Gift("Harry Potter and the Philosopher's Stone", 2);
+
+        System.out.println("Gift's name: " + book.getName());
+        System.out.println("Gift's weight: " + book.getWeight());
+
+        System.out.println("Gift: " + book);
+    }
+}
+
+class Gift {
+    private String bookName;
+    private int kg;
+    
+    public Gift (String bookName, int kg) {
+    this.bookName = bookName;
+    this.kg = kg;
+    }
+
+    public String getName() {
+        return bookName;
+    }
+
+    public int getWeight() {
+        return kg;
+    }
+
+    public String toString() {
+        return bookName + "(" + kg + ")";
+    }
+}
