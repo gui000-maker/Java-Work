ca681f3c107eae74184492000a8d115598c59c2d
gui <gui151208@gmail.com>
2025-11-10 20:37:23 -0300

    mooc exercise

---
+++ src/PackageGift.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class PackageGift {
+    static void main() {
+        Gift book = new Gift("Harry Potter and the Philosopher's Stone", 2);
+
+        Package gifts = new Package();
+        gifts.addGift(book);
+        System.out.println(gifts.totalWeight());
+    }
+}
+
+class Package {
+    private ArrayList<Gift> gifts = new ArrayList<>();
+    public int totalWeight;
+
+    public void addGift(Gift gift) {
+        totalWeight += gift.getWeight();
+        this.gifts.add(gift);
+    }
+
+    public int totalWeight() {
+        return totalWeight;
+    }
+}
