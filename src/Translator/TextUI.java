327252f40006ba1532aa6b08ab2d0947e4ad4da2
gui <gui151208@gmail.com>
2025-11-12 15:48:48 -0300

    mooc exercise

--- src/Translator/TextUI.java
+++ src/Translator/TextUI.java
@@ -4,22 +4,29 @@ import java.util.Scanner;
 
 public class TextUI {
     private final Scanner scanner;
-    private SimpleDictionary dictionary;
+    private final SimpleDictionary dictionary;
 
     public TextUI(Scanner scanner, SimpleDictionary dictionary) {
         this.scanner = scanner;
+        this.dictionary = dictionary;
     }
 
     public void start() {
-        System.out.println("Command: ");
+        System.out.print("Command: ");
         String word = scanner.nextLine();
 
         if (word.equals("end")) {
-            System.out.println("Bye bye!");
+            System.out.print("Bye bye!");
             return;
         } else if (word.equals("add")) {
-            System.out.println("Word: ");
+            System.out.print("Word: ");
+            word = scanner.nextLine();
 
+            System.out.println("Translation: ");
+            String translation = scanner.nextLine();
+
+            dictionary.add(word, translation);
+            start();
         }
         System.out.println("Unknown command");
         start();
