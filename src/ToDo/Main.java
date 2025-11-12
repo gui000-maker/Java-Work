871e6f1bbed69fd86b40c3e737bc8bfbfbe5372d
gui <gui151208@gmail.com>
2025-11-12 16:25:37 -0300

    mooc exercise

---
+++ src/ToDo/Main.java
@@ -0,0 +1,18 @@
+package ToDo;
+
+public class Main {
+    static void main() {
+        TodoList list = new TodoList();
+        list.add("read the course material");
+        list.add("watch the latest fool us");
+        list.add("take it easy");
+        list.print();
+        list.remove(2);
+        list.print();
+        list.add("buy raisins");
+        list.print();
+        list.remove(1);
+        list.remove(1);
+        list.print();
+    }
+}
---
+++ src/ToDo/TodoList.java
@@ -0,0 +1,21 @@
+package ToDo;
+
+import java.util.ArrayList;
+
+public class TodoList {
+    private final ArrayList<String> list = new ArrayList<>();
+
+    public void add(String task) {
+        list.add(task);
+    }
+
+    public void print() {
+        for (String task : list) {
+            System.out.println((list.indexOf(task) + 1) + ": " + task);
+        }
+    }
+
+    public void remove(int number) {
+        list.remove(number);
+    }
+}
