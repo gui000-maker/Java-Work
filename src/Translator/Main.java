74c60b5f90a23c67d122ba3600008fdb3551f44c
gui <gui151208@gmail.com>
2025-11-12 15:49:07 -0300

    mooc exercise

--- src/Translator/Main.java
+++ src/Translator/Main.java
@@ -4,15 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     static void main() {
-  /*
-        SimpleDictionary book = new SimpleDictionary();
-//        book.add("one", "yksi");
-//        book.add("two", "kaksi");
-
-        System.out.println(book.translate("one"));
-        System.out.println(book.translate("two"));
-        System.out.println(book.translate("three"));
-*/
         Scanner scanner = new Scanner(System.in);
         SimpleDictionary dictionary = new SimpleDictionary();
 
