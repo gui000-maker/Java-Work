1cec18f01c0203d4d85a9755c1028ab0d295a3e6
gui <gui151208@gmail.com>
2025-11-15 13:10:45 -0300

    mooc exercise

--- src/LiquidContainers/OOPContainers.java
+++ src/LiquidContainers/OOPContainers.java
@@ -1,18 +1,44 @@
 package LiquidContainers;
 
+import java.util.Scanner;
+
 public class OOPContainers {
     static void main() {
-        Container container = new Container();
-        System.out.println(container);
+        Container container1 = new Container();
+        Container container2 = new Container();
+        Container[] containers = {container1, container2};
+        Scanner scanner = new Scanner(System.in);
 
-        container.add(50);
-        System.out.println(container);
-        System.out.println(container.contains());
+        while (true) {
+            System.out.println(containers[0] + containers[1]);
 
-        container.remove(60);
-        System.out.println(container);
+            String input = scanner.nextLine();
+            String[] parts = input.split(" ");
 
-        container.add(200);
-        System.out.println(container);
+            String command = parts[0];
+            int amount = Integer.parseInt(parts[1]);
+
+            switch (command) {
+                case "add":
+                    containers[0].add(amount);
+                    break;
+                case "move":
+                    if (amount > 0) {
+                        if (amount > containers[0].contains()) {
+                            amount = containers[0].contains();
+                        }
+                        containers[0].remove(amount);
+                        containers[1].add(amount);
+                    }
+                    break;
+                case "remove":
+                    if (amount > 0) {
+                        containers[1].remove(amount);
+                        break;
+                    }
+                    case "quit":
+                    return;
+            }
+        }
     }
 }
