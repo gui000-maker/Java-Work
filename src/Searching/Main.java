7edfce13b8af550ef79091e791d76edaeb5e3338
gui <gui151208@gmail.com>
2025-11-16 17:11:49 -0300

    mooc exercise

--- src/Searching/Main.java
+++ src/Searching/Main.java
@@ -1,4 +1,19 @@
 package Searching;
 
+import java.util.ArrayList;
+
 public class Main {
+    static void main() {
+
+    }
+
+
+    public static int linearSearch(ArrayList<Book> books, int searchedId) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getId() == searchedId) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
