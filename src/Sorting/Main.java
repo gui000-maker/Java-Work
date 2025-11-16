3243c3c05e7389a55987695e1f81464021f80431
gui <gui151208@gmail.com>
2025-11-16 14:29:28 -0300

    mooc exercise

--- src/Sorting/Main.java
+++ src/Sorting/Main.java
@@ -1,11 +1,28 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class Main {
     static void main() {
-        int[] numbers = {-1, 6, 9, 8, 12};
-        System.out.println(Main.indexOfSmallestFrom(numbers, 0));
-        System.out.println(Main.indexOfSmallestFrom(numbers, 1));
-        System.out.println(Main.indexOfSmallestFrom(numbers, 2));
+        int[] numbers = {3, 2, 5, 4, 8};
+
+        System.out.println(Arrays.toString(numbers));
+
+        Main.swap(numbers, 1, 0);
+        System.out.println(Arrays.toString(numbers));
+
+        Main.swap(numbers, 0, 3);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public static int smallest(int[] array) {
+        int smallest = array[0];
+        for (int number : array) {
+            if (smallest > number) {
+                smallest = number;
+            }
+        }
+        return smallest;
     }
 
     private static int indexOfSmallest(int[] numbers) {
@@ -28,13 +45,9 @@ public class Main {
         return smallestIndex;
     }
 
-    public static int smallest(int[] array) {
-        int smallest = array[0];
-        for (int number : array) {
-            if (smallest > number) {
-                smallest = number;
-            }
-        }
-        return smallest;
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
 }
