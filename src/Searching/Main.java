17ddf1297e91819955d1a88cc68abc7e862d41b0
gui <gui151208@gmail.com>
2025-11-16 17:30:58 -0300

    mooc exercise

--- src/Searching/Main.java
+++ src/Searching/Main.java
@@ -16,4 +16,26 @@ public class Main {
         }
         return -1;
     }
+
+    public static int binarySearch(ArrayList<Book> books, int searchedId) {
+        int begin = 0;
+        int end = books.size() - 1;
+        int middle;
+        int bookId;
+
+        while (begin <= end) {
+            middle = (end + begin) / 2;
+            bookId = books.get(middle).getId();
+
+            if (bookId == searchedId) {
+                return middle;
+            } else if (bookId < searchedId) {
+                begin = middle +1;
+            }
+            else {
+                end = middle -1;
+            }
+        }
+        return -1;
+    }
 }
