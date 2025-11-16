895f552ac9c09421d1dcd5c7d51dadfadd779a90
gui <gui151208@gmail.com>
2025-11-16 14:16:59 -0300

    mooc exercise

--- src/Sorting/Main.java
+++ src/Sorting/Main.java
@@ -2,14 +2,26 @@ package Sorting;
 
 public class Main {
     static void main() {
-        int[] numbers = {6, 5, 8, 7, 11};
-        System.out.println("Index of the smallest number: " + Main.indexOfSmallest(numbers));
+        int[] numbers = {-1, 6, 9, 8, 12};
+        System.out.println(Main.indexOfSmallestFrom(numbers, 0));
+        System.out.println(Main.indexOfSmallestFrom(numbers, 1));
+        System.out.println(Main.indexOfSmallestFrom(numbers, 2));
     }
 
     private static int indexOfSmallest(int[] numbers) {
         int smallestIndex = 0;
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[smallestIndex] > numbers[i]) {
+                smallestIndex = i;
+            }
+        }
+        return smallestIndex;
+    }
+
+    public static int indexOfSmallestFrom(int[] table, int startIndex) {
+        int smallestIndex = startIndex;
+        for (int i = startIndex; i < table.length; i++) {
+            if (table[smallestIndex] > table[i]) {
                 smallestIndex = i;
             }
         }
