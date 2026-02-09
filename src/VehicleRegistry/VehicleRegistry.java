d5a7a8c22849c0b13b2adfba16b1b843e2477291
gui <gui151208@gmail.com>
2026-02-09 16:39:00 -0300

    mooc exercise

---
+++ src/VehicleRegistry/VehicleRegistry.java
@@ -0,0 +1,50 @@
+package VehicleRegistry;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class VehicleRegistry {
+    HashMap <LicensePlate, String> owners;
+
+    public VehicleRegistry() {
+        this.owners = new HashMap<>();
+    }
+
+    public boolean add(LicensePlate licensePlate, String owner) {
+        if (this.owners.containsKey(licensePlate)) {
+            return false;
+        }
+
+        this.owners.put(licensePlate, owner);
+        return true;
+    }
+
+    public String get(LicensePlate licensePlate) {
+        return this.owners.get(licensePlate);
+    }
+
+    public boolean remove(LicensePlate licensePlate) {
+        if (!this.owners.containsKey(licensePlate)) {
+            return false;
+        }
+
+        this.owners.remove(licensePlate);
+        return true;
+    }
+
+    public void printLicensePlates() {
+        for (LicensePlate licensePlate : this.owners.keySet()) {
+            System.out.println(licensePlate);
+        }
+    }
+
+    public void printOwners() {
+        ArrayList<String> printedOwners = new ArrayList<>();
+        for (String owner : this.owners.values()) {
+            if (!printedOwners.contains(owner)) {
+                System.out.println(owner);
+                printedOwners.add(owner);
+            }
+        }
+    }
+}
