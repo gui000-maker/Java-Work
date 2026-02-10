89adc8f4d9bde5fe1398b303a20fb12478d3dffe
gui <gui151208@gmail.com>
2026-02-10 17:41:47 -0300

    mooc exercise

---
+++ src/StorageFacility/Main.java
@@ -0,0 +1,19 @@
+package StorageFacility;
+
+public class Main {
+    static void main() {
+        StorageFacility facility = new StorageFacility();
+        facility.add("a14", "ice skates");
+        facility.add("a14", "ice hockey stick");
+        facility.add("a14", "ice skates");
+
+        facility.add("f156", "rollerblades");
+        facility.add("f156", "rollerblades");
+
+        facility.add("g63", "six");
+        facility.add("g63", "pi");
+
+        System.out.println(facility.contents("a14"));
+        System.out.println(facility.contents("f156"));
+    }
+}
---
+++ src/StorageFacility/StorageFacility.java
@@ -0,0 +1,39 @@
+package StorageFacility;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class StorageFacility {
+    HashMap<String, ArrayList<String>> storage;
+
+    public StorageFacility() {
+        this.storage = new HashMap<>();
+    }
+
+    public void add(String unit, String item) {
+        if (!this.storage.containsKey(unit)) {
+            this.storage.put(unit, new ArrayList<>());
+            this.storage.get(unit).add(item);
+        } else {
+            this.storage.get(unit).add(item);
+        }
+    }
+
+    public ArrayList<String> contents(String storageUnit) {
+        if (!this.storage.containsKey(storageUnit)) {
+            return new ArrayList<>();
+        } else {
+            return this.storage.get(storageUnit);
+        }
+    }
+
+    public void remove(String storageUnit, String item) {
+        if (!this.storage.containsKey(storageUnit))
+            return;
+    }
+    else
+
+    {
+
+    }
+}
