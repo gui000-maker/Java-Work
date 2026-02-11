7730f34c6d3bd005fdf48f0566ad7022d049f844
gui <gui151208@gmail.com>
2026-02-11 17:22:18 -0300

    mooc exercise

--- src/PersonSub/Teacher.java
+++ src/PersonSub/Teacher.java
@@ -1,4 +1,15 @@
 package PersonSub;
 
-public class Teacher {
+public class Teacher extends Person {
+        private int salary;
+
+        public Teacher(String name, String local, int salary) {
+            super(name, local);
+            this.salary = salary;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + "\n  salary " + salary + " euro/month";
+        }
 }
