97717bf9b03190ecca883d05d18e80e772d1d279
gui <gui151208@gmail.com>
2026-02-11 17:13:29 -0300

    mooc exercise

--- src/PersonSub/Student.java
+++ src/PersonSub/Student.java
@@ -1,4 +1,17 @@
 package PersonSub;
 
-public class Student {
+public class Student extends Person {
+    private int credits;
+
+    public Student (String item, String local) {
+        super(item, local);
+    }
+
+    public void study() {
+        this.credits++;
+    }
+
+    public int credits() {
+        return this.credits;
+    }
 }
