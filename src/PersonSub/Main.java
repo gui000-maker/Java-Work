217403ab5b1e77098f2f72e5f65e77fefc5a25ca
gui <gui151208@gmail.com>
2026-02-11 17:28:06 -0300

    mooc exercise

--- src/PersonSub/Main.java
+++ src/PersonSub/Main.java
@@ -1,19 +1,19 @@
 package PersonSub;
 
+import java.util.ArrayList;
+
 public class Main {
     static void main() {
-        Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
-        Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
-        System.out.println(ada);
-        System.out.println(esko);
+        ArrayList<Person> persons = new ArrayList<>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
 
-        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+        printPersons(persons);
+    }
 
-        int i = 0;
-        while (i < 25) {
-            ollie.study();
-            i = i + 1;
+    private static void printPersons(ArrayList<Person> persons) {
+        for (Person person : persons) {
+            System.out.println(person);
         }
-        System.out.println(ollie);
     }
 }
