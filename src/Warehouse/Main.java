52b56e93a758b234acbfba00ade0f2ac9f3635db
gui <gui151208@gmail.com>
2026-02-11 18:31:25 -0300

    mooc exercise

--- src/Warehouse/Main.java
+++ src/Warehouse/Main.java
@@ -2,17 +2,11 @@ package Warehouse;
 
 public class Main {
     static void main() {
-// the usual:
         ProductWarehouseWithHistory juice = new ProductWarehouseWithHistory("Juice", 1000.0, 1000.0);
         juice.takeFromWarehouse(11.3);
-        System.out.println(juice.getName()); // Juice
         juice.addToWarehouse(1.0);
-        System.out.println(juice);           // Juice: balance = 989.7, space left 10.3
+//System.out.println(juice.history()); // [1000.0, 988.7, 989.7]
 
-// etc
-
-// however, history() still doesn't work properly:
-        System.out.println(juice.history()); // [1000.0]
-// so we only get the initial state of the history set by the constructor...
+        juice.printAnalysis();
     }
 }
--- src/Warehouse/ProductWarehouseWithHistory.java
+++ src/Warehouse/ProductWarehouseWithHistory.java
@@ -5,12 +5,32 @@ public class ProductWarehouseWithHistory extends ProductWarehouse {
 
     public ProductWarehouseWithHistory(String productName, double capacity, double initialBalance) {
         super(productName, capacity);
+        this.history = new ChangeHistory();
         addToWarehouse(initialBalance);
-        history = new ChangeHistory();
-        history.add(getBalance());
     }
 
     public String history() {
         return history.toString();
+    }
+
+    @Override
+    public void addToWarehouse(double amount) {
+        super.addToWarehouse(amount);
+        history.add(getBalance());
+    }
+
+    @Override
+    public double takeFromWarehouse(double amount) {
+        super.takeFromWarehouse(amount);
+        history.add(getBalance());
+        return getBalance();
+    }
+
+    public void printAnalysis() {
+        System.out.println("Product: " + getName());
+        System.out.println("History: " + history);
+        System.out.println("Largest amount of product: " + history.maxValue());
+        System.out.println("Smallest amount of product: " + history.minValue());
+        System.out.println("Average: " + history.average());
     }
 }
