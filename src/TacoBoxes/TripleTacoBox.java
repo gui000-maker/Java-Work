0089abef07aa398518eb9ba846519fa8f85be023
gui <gui151208@gmail.com>
2026-02-12 20:28:16 -0300

    mooc exercise

---
+++ src/TacoBoxes/TripleTacoBox.java
@@ -0,0 +1,19 @@
+package TacoBoxes;
+
+public class TripleTacoBox implements TacoBox {
+    private int tacos;
+
+    public TripleTacoBox() {
+        this.tacos = 3;
+    }
+
+    public int tacosRemaining() {
+        return this.tacos;
+    }
+
+    public void eat() {
+        if (this.tacos > 0) {
+            this.tacos--;
+        }
+    }
+}
