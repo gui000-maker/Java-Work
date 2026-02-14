ff61cc992894e6e06d14dcfc8ba7310cb2a6de0c
gui <gui151208@gmail.com>
2026-02-14 15:12:00 -0300

    mooc exercise

--- src/OnlineShop/Warehouse.java
+++ src/OnlineShop/Warehouse.java
@@ -3,16 +3,34 @@ package OnlineShop;
 import java.util.Map;
 
 public class Warehouse {
-    private final Map<String, Integer> products = new java.util.HashMap<>();
+    private final Map<String, Integer> productsPrices = new java.util.HashMap<>();
+    private final Map<String, Integer> productsStock = new java.util.HashMap<>();
 
     public void addProduct(String product, int price, int stock) {
-        products.put(product, price);
+        productsPrices.put(product, price);
+        productsStock.put(product, stock);
     }
 
-    public int price(String productName) {
-        if (products.containsKey(productName)) {
-            return products.get(productName);
+    public int price(String product) {
+        if (productsPrices.containsKey(product)) {
+            return productsPrices.get(product);
         }
         return -99;
+    }
+
+    public int stock(String product) {
+        if (productsStock.containsKey(product)) {
+            return productsStock.get(product);
+        }
+        return 0;
+    }
+
+    public boolean take(String product) {
+        if (!(productsStock.containsKey(product)) || productsStock.get(product) == 0) {
+            return false;
+        }
+
+        productsStock.put(product, productsStock.get(product) - 1);
+        return true;
     }
 }
